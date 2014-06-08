725fc72619a458a1a15b0620965193f00b579914
Christopher Schwan <schwan@uni-mainz.de>
2014-06-08 14:57:59 +0200

    Add test for `vegas_adapt_grid`

--- tests/test_vegas.cpp
+++ tests/test_vegas.cpp
@@ -60,6 +60,46 @@ typedef testing::Types<float, double, long double> MyT;
 template <typename T> class Vegas : public testing::Test { };
 TYPED_TEST_CASE(Vegas, MyT);
 
+TYPED_TEST(Vegas, CheckAdjustGrid)
+{
+	typedef TypeParam T;
+
+	// uniform distribution
+	hep::piecewise_constant_pdf<T> old_grid(1, 4);
+
+	// with this data it should remain a uniform distribution
+	std::vector<T> adjustment_data = { T(1.0), T(1.0), T(1.0), T(1.0), T(), T() };
+	auto new_grid = hep::vegas_adjust_grid(T(1.5), old_grid, adjustment_data);
+
+	EXPECT_EQ( old_grid(0, 0), new_grid(0, 0) );
+	EXPECT_EQ( old_grid(0, 1), new_grid(0, 1) );
+	EXPECT_EQ( old_grid(0, 2), new_grid(0, 2) );
+	EXPECT_EQ( old_grid(0, 3), new_grid(0, 3) );
+
+	// this should make the first bin(s) smaller
+	adjustment_data = { T(4.0), T(1.0), T(1.0), T(1.0), T(), T() };
+	new_grid = hep::vegas_adjust_grid(T(1.0), old_grid, adjustment_data);
+
+	EXPECT_FLOAT_EQ( T(2.072390895598839e-01), new_grid(0, 0) );
+	EXPECT_FLOAT_EQ( T(4.303460252668945e-01), new_grid(0, 1) );
+	EXPECT_FLOAT_EQ( T(7.047478748905420e-01), new_grid(0, 2) );
+	EXPECT_FLOAT_EQ( T(1.000000000000000e+00), new_grid(0, 3) );
+
+	new_grid = hep::vegas_adjust_grid(T(1.5), old_grid, adjustment_data);
+
+	EXPECT_FLOAT_EQ( T(1.904424247771927e-01), new_grid(0, 0) );
+	EXPECT_FLOAT_EQ( T(4.002750242439110e-01), new_grid(0, 1) );
+	EXPECT_FLOAT_EQ( T(6.761486310730158e-01), new_grid(0, 2) );
+	EXPECT_FLOAT_EQ( T(1.000000000000000e+00), new_grid(0, 3) );
+
+	new_grid = hep::vegas_adjust_grid(T(2.0), old_grid, adjustment_data);
+
+	EXPECT_FLOAT_EQ( T(1.760695619039624e-01), new_grid(0, 0) );
+	EXPECT_FLOAT_EQ( T(3.727972665008580e-01), new_grid(0, 1) );
+	EXPECT_FLOAT_EQ( T(6.426226726635829e-01), new_grid(0, 2) );
+	EXPECT_FLOAT_EQ( T(1.000000000000000e+00), new_grid(0, 3) );
+}
+
 TYPED_TEST(Vegas, IntegrateSquareFunction)
 {
 	typedef TypeParam T;
