cschwan/hep-mc

commit 3e298e96445d9e03e51390d283370695e59191da

Switch on single generator by default
- if using MPI for parallel integration the exact numerical result
  depends on the number of processors - this is a problem if
  reproducability is desired but the feature haven't switched on before
  integration. Since it isn't a bottleneck we switch it on by default.
  This has the additional advantage that the state of the RNGs on all
  processors are in the same state after integration and leaves seeding
  the RNG again to to user - this isn't the MC's business

include/hep/mc/mpi_vegas.hpp
@@ -3,7 +3,7 @@
 
 /*
  * hep-mc - A Template Library for Monte Carlo Integration
- * Copyright (C) 2013-2014  Christopher Schwan
+ * Copyright (C) 2013-2015  Christopher Schwan
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -58,14 +58,6 @@ inline std::vector<vegas_iteration_result<T>> mpi_vegas(
 	int world = 0;
 	MPI_Comm_size(communicator, &world);
 
-	if (!mpi_single_generator())
-	{
-		// seed every random number generator differently
-		std::size_t const r = rank * 10;
-		std::seed_seq sequence{r+0, r+1, r+2, r+3, r+4, r+5, r+6, r+7, r+8, r+9};
-		generator.seed(sequence);
-	}
-
 	// create a fresh grid
 	auto pdf = start_pdf;
 
@@ -73,22 +65,18 @@ inline std::vector<vegas_iteration_result<T>> mpi_vegas(
 	std::vector<vegas_iteration_result<T>> results;
 	results.reserve(iteration_calls.size());
 
+	std::size_t const usage = pdf.dimensions() * random_number_usage<T, R>();
+
 	// perform iterations
 	for (auto i = iteration_calls.begin(); i != iteration_calls.end(); ++i)
 	{
-		if (mpi_single_generator())
-		{
-			advance_generator_before<T>(pdf.dimensions(), *i, rank, world, generator);
-		}
+		generator.discard(usage * discard_before(*i, rank, world));
 
 		std::size_t const calls = (*i / world) +
 			(static_cast <std::size_t> (rank) < (*i % world) ? 1 : 0);
 		auto result = vegas_iteration(calls, *i, pdf, function, generator);
 
-		if (mpi_single_generator())
-		{
-			advance_generator_after<T>(pdf.dimensions(), *i, calls, rank, world, generator);
-		}
+		generator.discard(usage * discard_after(*i, calls, rank, world));
 
 		// add up results
 		MPI_Allreduce(

examples/mpi_vegas_example.cpp
@@ -61,9 +61,6 @@ int main(int argc, char* argv[])
 		std::cout << "running hep::mpi_vegas with " << size << " processes\n\n";
 	}
 
-	// use a single generator: slower, but makes the result independent of the number of processes
-	hep::mpi_single_generator(true);
-
 	// set the verbose vegas callback function
 	hep::mpi_vegas_callback<double>(hep::mpi_vegas_verbose_callback<double>);
 

include/hep/mc/generator_helper.hpp
@@ -3,7 +3,7 @@
 
 /*
  * hep-mc - A Template Library for Monte Carlo Integration
- * Copyright (C) 2014  Christopher Schwan
+ * Copyright (C) 2014-2015  Christopher Schwan
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -45,40 +45,28 @@ inline std::size_t random_number_usage()
 	return k;
 }
 
-template <typename T, typename R>
-inline void advance_generator_before(
-	std::size_t dimensions,
+inline std::size_t discard_before(
 	std::size_t total_calls,
 	std::size_t rank,
-	std::size_t world,
-	R& generator
+	std::size_t world
 ) {
-	std::size_t const usage = random_number_usage<T, R>() * dimensions;
-
 	std::size_t const before = (total_calls / world) * rank +
 		(((total_calls % world) < rank) ? total_calls % world : rank);
 
-	generator.discard(before * usage);
+	return before;
 }
 
-template <typename T, typename R>
-inline void advance_generator_after(
-	std::size_t dimensions,
+inline std::size_t discard_after(
 	std::size_t total_calls,
 	std::size_t calls,
 	std::size_t rank,
-	std::size_t world,
-	R& generator
+	std::size_t world
 ) {
-	std::size_t const usage = random_number_usage<T, R>() * dimensions;
 
-	std::size_t const before = (total_calls / world) * rank +
-		(((total_calls % world) < rank) ? total_calls % world : rank);
+	std::size_t const before = discard_before(total_calls, rank, world);
+	std::size_t const after = ((before + calls) < total_calls) ? (total_calls - before - calls) : 0;
 
-	std::size_t const after = ((before + calls) < total_calls) ?
-		(total_calls - before - calls) : 0;
-
-	generator.discard(after * usage);
+	return after;
 }
 
 }

include/hep/mc/global_configuration.hpp
@@ -3,7 +3,7 @@
 
 /*
  * hep-mc - A Template Library for Monte Carlo Integration
- * Copyright (C) 2014  Christopher Schwan
+ * Copyright (C) 2014-2015  Christopher Schwan
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,33 +24,6 @@ namespace hep
 
 /// \addtogroup global_configuration
 /// @{
-
-/**
- * This function returns `true` the MPI functions use a single random number generator. This means
- * that the generator of each processor discards an appropriate amount of random numbers to
- * ascertain independent numbers are used. This make sure the result is independent of the number
- * of processesor and the same as the non-MPI routine. Since each process consumes as many random
- * numbers as calls are made in total, this may slow down the integration if the integrand evaluates
- * too fast. In this case a faster random number generator should be used.
- */
-inline bool& mpi_single_generator()
-{
-	static bool use_single_generator = false;
-
-	return use_single_generator;
-}
-
-/**
- * If `enabled` is `true` the MPI integration routines use a single random number generator so that
- * results are independent from the number of processors, in particular the same result than the
- * non-MPI integrators yield.
- *
- * \see \ref mpi_single_generator
- */
-inline void mpi_single_generator(bool enabled)
-{
-	mpi_single_generator() = enabled;
-}
 
 /**
  * Returns `true` if the VEGAS routines should use the grid refinement method of CUBA \cite Cuba for

include/hep/mc/mpi_plain.hpp
@@ -3,7 +3,7 @@
 
 /*
  * hep-mc - A Template Library for Monte Carlo Integration
- * Copyright (C) 2013-2014  Christopher Schwan
+ * Copyright (C) 2013-2015  Christopher Schwan
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -65,14 +65,6 @@ inline mc_result<T> mpi_plain(
 	int world = 0;
 	MPI_Comm_size(communicator, &world);
 
-	if (!mpi_single_generator())
-	{
-		// seed every random number generator differently
-		std::size_t const r = rank * 10;
-		std::seed_seq sequence{r+0, r+1, r+2, r+3, r+4, r+5, r+6, r+7, r+8, r+9};
-		generator.seed(sequence);
-	}
-
 	// default-initialize sum and sum_of_squares
 	T  buffer[2]      = { T(), T() };
 	T& sum            = buffer[0];
@@ -82,30 +74,19 @@ inline mc_result<T> mpi_plain(
 	std::size_t const sub_calls = (calls / world) +
 		(static_cast <std::size_t> (rank) < (calls % world) ? 1 : 0);
 
-	if (mpi_single_generator())
-	{
-		advance_generator_before<T>(dimensions, calls, rank, world, generator);
-	}
+	std::size_t const usage = dimensions * random_number_usage<T, R>();
+
+	generator.discard(usage * discard_before(calls, rank, world));
 
 	auto result = plain_iteration<T>(dimensions, calls, sub_calls, function, generator);
+
+	generator.discard(usage * discard_after(calls, sub_calls, rank, world));
 
 	sum = result.value * T(result.calls);
 	sum_of_squares = T(result.calls) * (result.value * result.value + T(calls) *
 		result.error * result.error);
 
-	if (mpi_single_generator())
-	{
-		advance_generator_after<T>(dimensions, calls, sub_calls, rank, world, generator);
-	}
-
-	MPI_Allreduce(
-		MPI_IN_PLACE,
-		&buffer,
-		2,
-		mpi_datatype<T>(),
-		MPI_SUM,
-		communicator
-	);
+	MPI_Allreduce(MPI_IN_PLACE, &buffer, 2, mpi_datatype<T>(), MPI_SUM, communicator);
 
 	return mc_result<T>(calls, sum, sum_of_squares);
 }

commit 43cf0433462bac1d8be2bffd53963e9c8b4fdbfd

Fix for compilation error
- problem was introduced with commit 3e298e9
- damn!

include/hep/mc/mpi_vegas.hpp
@@ -29,6 +29,7 @@
 
 #include <cstddef>
 #include <random>
+#include <utility>
 #include <vector>
 
 #include <mpi.h>
@@ -140,7 +141,7 @@ inline std::vector<vegas_iteration_result<T>> mpi_vegas(
 		function,
 		vegas_pdf<T>(dimensions, bins),
 		alpha,
-		generator
+		std::forward<R>(generator)
 	);
 }
 

include/hep/mc/vegas.hpp
@@ -3,7 +3,7 @@
 
 /*
  * hep-mc - A Template Library for Monte Carlo Integration
- * Copyright (C) 2012-2014  Christopher Schwan
+ * Copyright (C) 2012-2015  Christopher Schwan
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,6 +27,7 @@
 #include <cstddef>
 #include <limits>
 #include <random>
+#include <utility>
 #include <vector>
 
 namespace hep
@@ -168,7 +169,7 @@ inline std::vector<vegas_iteration_result<T>> vegas(
 		function,
 		vegas_pdf<T>(dimensions, bins),
 		alpha,
-		generator
+		std::forward<R>(generator)
 	);
 }
 