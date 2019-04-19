12fdb165b0bf054c7504bc909363cda4a5209a4a
Christopher Schwan <handgranaten-herbert@posteo.de>
2019-04-19 13:27:30 +0200

    Migrate `test_plain_chkpt` to Catch2
    - improved tests

--- tests/test_plain_chkpt.cpp
+++ tests/test_plain_chkpt.cpp
@@ -1,13 +1,13 @@
-#include "gtest/gtest.h"
+#include "hep/mc/integrand.hpp"
+#include "hep/mc/plain.hpp"
 
-#include "hep/mc.hpp"
+#include "catch2/catch.hpp"
 
 #include <cstddef>
-#include <fstream>
+#include <sstream>
 #include <vector>
 
-using T = double;
-
+template <typename T>
 T linear_function(hep::mc_point<T> const& point, hep::projector<T>& projector)
 {
     T const x = point.point().at(0);
@@ -18,14 +18,16 @@ T linear_function(hep::mc_point<T> const& point, hep::projector<T>& projector)
     return v;
 }
 
-TEST(TestPlainCheckpoint, Bla)
+TEMPLATE_TEST_CASE("plain_chkpt serialization", "", float, double, long double)
 {
+    using T = TestType;
+
     // create a checkpoint using five iterations with PLAIN
     auto const chkpt1 = hep::plain(
         hep::make_integrand<T>(
-            linear_function,
+            linear_function<T>,
             1,
-            hep::make_dist_params<T>(10, T(0.0), T(1.0), "distribution name")
+            hep::make_dist_params<T>(10, T(0.0), T(1.0), "distribution #1")
         ),
         std::vector<std::size_t>(5, 1000)
     );
@@ -43,5 +45,61 @@ TEST(TestPlainCheckpoint, Bla)
     chkpt2.serialize(stream2);
 
     // string representation should be the same
-    ASSERT_STREQ( stream1.str().c_str() , stream2.str().c_str() );
+    CHECK( stream1.str() == stream2.str() );
+}
+
+TEMPLATE_TEST_CASE("plain_chkpt series", "", float, double, long double)
+{
+    using T = TestType;
+
+    // check if we get the same checkpoint when we perform five iterations in one go, and when we
+    // perfom first one iteration, then resume the resulting checkpoint with three iterations, and
+    // finally resume it with one iteration
+
+    auto const chkptA = hep::plain(
+        hep::make_integrand<T>(
+            linear_function<T>,
+            1,
+            hep::make_dist_params<T>(10, T(0.0), T(1.0), "distribution #1")
+        ),
+        std::vector<std::size_t>(5, 1000)
+    );
+
+    auto const tmp_chkpt1 = hep::plain(
+        hep::make_integrand<T>(
+            linear_function<T>,
+            1,
+            hep::make_dist_params<T>(10, T(0.0), T(1.0), "distribution #1")
+        ),
+        std::vector<std::size_t>(1, 1000)
+    );
+
+    auto const tmp_chkpt2 = hep::plain(
+        hep::make_integrand<T>(
+            linear_function<T>,
+            1,
+            hep::make_dist_params<T>(10, T(0.0), T(1.0), "distribution #1")
+        ),
+        std::vector<std::size_t>(3, 1000),
+        tmp_chkpt1
+    );
+
+    auto const chkptB = hep::plain(
+        hep::make_integrand<T>(
+            linear_function<T>,
+            1,
+            hep::make_dist_params<T>(10, T(0.0), T(1.0), "distribution #1")
+        ),
+        std::vector<std::size_t>(1, 1000),
+        tmp_chkpt2
+    );
+
+    // Check if both checkpoints are the same
+    std::ostringstream stream1;
+    std::ostringstream stream2;
+
+    chkptA.serialize(stream1);
+    chkptB.serialize(stream2);
+
+    CHECK( stream1.str() == stream2.str() );
 }
