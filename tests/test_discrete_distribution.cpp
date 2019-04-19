b86280d0ab7eb7e1df0723c90a8bb4b2287bdebf
Christopher Schwan <handgranaten-herbert@posteo.de>
2019-04-19 12:57:28 +0200

    Migrate `test_discrete_distribution` to Catch2

--- tests/test_discrete_distribution.cpp
+++ tests/test_discrete_distribution.cpp
@@ -1,118 +1,76 @@
-#include "gtest/gtest.h"
-
 #include "hep/mc/discrete_distribution.hpp"
 
+#include "catch2/catch.hpp"
+
 #include <cstddef>
+#include <limits>
 #include <random>
 #include <vector>
 
-typedef testing::Types<float, double, long double> MyT;
-template <typename T> class DiscreteDistribution : public testing::Test { };
-TYPED_TEST_CASE(DiscreteDistribution, MyT);
-
-TYPED_TEST(DiscreteDistribution, DistributionWithOneBin)
+TEMPLATE_TEST_CASE("discrete_distribution with no bins", "", float, double, long double)
 {
-    typedef TypeParam T;
+    using T = TestType;
 
-    std::mt19937 rng;
-    std::vector<T> weights = { T(1.0) };
-    hep::discrete_distribution<std::size_t, T> distribution(weights.begin(), weights.end());
+    std::mt19937_64 rng;
+    std::vector<T> weights;
 
-    // distribution with one bin should always give that one bin
-    EXPECT_EQ( distribution(rng) , 0u );
-    EXPECT_EQ( distribution(rng) , 0u );
-    EXPECT_EQ( distribution(rng) , 0u );
-    EXPECT_EQ( distribution(rng) , 0u );
-    EXPECT_EQ( distribution(rng) , 0u );
+    hep::discrete_distribution<std::size_t, T> distribution{weights.begin(), weights.end()};
+
+    CHECK( distribution(rng) == 0u );
+    CHECK( distribution(rng) == 0u );
+    CHECK( distribution(rng) == 0u );
+    CHECK( distribution(rng) == 0u );
+    CHECK( distribution(rng) == 0u );
 }
 
-TYPED_TEST(DiscreteDistribution, DistributionWithTwoEqualBins)
+TEMPLATE_TEST_CASE("discrete_distribution with one bin", "", float, double, long double)
 {
-    typedef TypeParam T;
+    using T = TestType;
 
     std::mt19937 rng;
-    std::vector<T> weights = { T(1.0), T(1.0) };
-    hep::discrete_distribution<std::size_t, T> distribution(weights.begin(), weights.end());
+    std::vector<T> weights{T(1.0)};
 
-    // distribution with one bin should always give that one bin
-    std::size_t frequencies[2] = { 0, 0 };
+    hep::discrete_distribution<std::size_t, T> distribution{weights.begin(), weights.end()};
 
-    std::size_t const count = 10000;
-    for (std::size_t i = 0; i != count; ++i)
-    {
-        std::size_t index = distribution(rng);
-
-        if (index >= 2)
-        {
-            FAIL() << "index out of bounds";
-        }
-
-        ++frequencies[index];
-    }
-
-    T const relative_frequencies[] = { T(frequencies[0]) / count, T(frequencies[1]) / count };
-
-    EXPECT_NEAR(relative_frequencies[0], T(0.5), T(1e-1));
-    EXPECT_NEAR(relative_frequencies[1], T(0.5), T(1e-1));
+    CHECK( distribution(rng) == 0u );
+    CHECK( distribution(rng) == 0u );
+    CHECK( distribution(rng) == 0u );
+    CHECK( distribution(rng) == 0u );
+    CHECK( distribution(rng) == 0u );
 }
 
-TYPED_TEST(DiscreteDistribution, DistributionWithTwoUnequalBins)
+TEMPLATE_TEST_CASE("discrete_distribution with two bins", "", float, double, long double)
 {
-    typedef TypeParam T;
+    using T = TestType;
 
-    std::mt19937 rng;
-    std::vector<T> weights = { T(3.0), T(1.0) };
-    hep::discrete_distribution<std::size_t, T> distribution(weights.begin(), weights.end());
+    // make sure that one random number suffices for all floating point types - otherwise the test
+    // results depend on the numeric type
+    REQUIRE( std::numeric_limits<std::mt19937_64::result_type>::digits >=
+        std::numeric_limits<T>::digits );
 
-    // distribution with one bin should always give that one bin
-    std::size_t frequencies[] = { 0, 0 };
+    std::mt19937_64 rng;
+    std::vector<T> weights{T(1.0), T(2.0)};
 
-    std::size_t const count = 10000;
-    for (std::size_t i = 0; i != count; ++i)
-    {
-        std::size_t index = distribution(rng);
+    hep::discrete_distribution<std::size_t, T> distribution{weights.begin(), weights.end()};
 
-        if (index >= 2)
-        {
-            FAIL() << "index out of bounds";
-        }
-
-        ++frequencies[index];
-    }
-
-    T const relative_frequencies[] = { T(frequencies[0]) / count, T(frequencies[1]) / count };
-
-    EXPECT_NEAR(relative_frequencies[0], T(0.75), T(1e-1));
-    EXPECT_NEAR(relative_frequencies[1], T(0.25), T(1e-1));
-}
-
-TYPED_TEST(DiscreteDistribution, DistributionWithOneHundredBins)
-{
-    typedef TypeParam T;
-
-    std::mt19937 rng;
-    std::vector<T> weights(100, T(1.0));
-    hep::discrete_distribution<std::size_t, T> distribution(weights.begin(), weights.end());
-
-    // distribution with one bin should always give that one bin
-    std::vector<std::size_t> frequencies(100, 0);
-
-    std::size_t const count = 100000;
-    for (std::size_t i = 0; i != count; ++i)
-    {
-        std::size_t index = distribution(rng);
-
-        if (index >= frequencies.size())
-        {
-            FAIL() << "index out of bounds";
-        }
-
-        ++frequencies[index];
-    }
-
-    for (std::size_t i = 0; i != frequencies.size(); ++i)
-    {
-        T const relative_frequency = T(frequencies[i]) / count;
-        EXPECT_NEAR(relative_frequency, T(1.0) / count, T(1e-1));
-    }
+    CHECK( distribution(rng) == 1u );
+    CHECK( distribution(rng) == 0u );
+    CHECK( distribution(rng) == 1u );
+    CHECK( distribution(rng) == 1u );
+    CHECK( distribution(rng) == 0u );
+    CHECK( distribution(rng) == 1u );
+    CHECK( distribution(rng) == 0u );
+    CHECK( distribution(rng) == 0u );
+    CHECK( distribution(rng) == 1u );
+    CHECK( distribution(rng) == 1u );
+    CHECK( distribution(rng) == 0u );
+    CHECK( distribution(rng) == 1u );
+    CHECK( distribution(rng) == 0u );
+    CHECK( distribution(rng) == 1u );
+    CHECK( distribution(rng) == 1u );
+    CHECK( distribution(rng) == 1u );
+    CHECK( distribution(rng) == 1u );
+    CHECK( distribution(rng) == 1u );
+    CHECK( distribution(rng) == 1u );
+    CHECK( distribution(rng) == 0u );
 }
