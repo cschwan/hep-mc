92bc672652d465b0765a71a4171cf238d7078477
Christopher Schwan <handgranaten-herbert@posteo.de>
2019-04-19 13:44:49 +0200

    Migrate `test_vegas_chkpt` to Catch2

--- tests/test_vegas_chkpt.cpp
+++ tests/test_vegas_chkpt.cpp
@@ -1,14 +1,14 @@
-#include "gtest/gtest.h"
+#include "hep/mc/integrand.hpp"
+#include "hep/mc/vegas.hpp"
 
-#include "hep/mc.hpp"
+#include "catch2/catch.hpp"
 
 #include <cstddef>
-#include <fstream>
+#include <sstream>
 #include <vector>
 
-using T = double;
-
-T linear_function(hep::mc_point<T> const& point, hep::projector<T>& projector)
+template <typename T>
+T linear_function(hep::vegas_point<T> const& point, hep::projector<T>& projector)
 {
     T const x = point.point().at(0);
     T const v = x;
@@ -18,14 +18,18 @@ T linear_function(hep::mc_point<T> const& point, hep::projector<T>& projector)
     return v;
 }
 
-TEST(TestVegasCheckpoint, test_empty_checkpoint)
+TEMPLATE_TEST_CASE("vegas_chkpt empty serialization", "", float, double, long double)
 {
-    // create a checkpoint using five iterations with PLAIN
-    auto chkpt1 = hep::make_vegas_chkpt<double>();
+    using T = TestType;
 
+    hep::vegas_chkpt<T> chkpt1 = hep::make_vegas_chkpt<T>();
+
+    // must be called otherwise program crashes
     chkpt1.dimensions(1);
 
-    // serialize checkpoint
+    // check default parameter
+    CHECK( chkpt1.alpha() == T(1.5) );
+
     std::ostringstream stream1;
     chkpt1.serialize(stream1);
 
@@ -38,17 +42,19 @@ TEST(TestVegasCheckpoint, test_empty_checkpoint)
     chkpt2.serialize(stream2);
 
     // string representation should be the same
-    ASSERT_STREQ( stream1.str().c_str() , stream2.str().c_str() );
+    CHECK( stream1.str() == stream2.str() );
 }
 
-TEST(TestVegasCheckpoint, test_non_empty_checkpoint)
+TEMPLATE_TEST_CASE("vegas_chkpt serialization", "", float, double, long double)
 {
+    using T = TestType;
+
     // create a checkpoint using five iterations with PLAIN
     auto const chkpt1 = hep::vegas(
         hep::make_integrand<T>(
-            linear_function,
+            linear_function<T>,
             1,
-            hep::make_dist_params<T>(10, T(0.0), T(1.0), "distribution name")
+            hep::make_dist_params<T>(10, T(0.0), T(1.0), "distribution #1")
         ),
         std::vector<std::size_t>(5, 1000)
     );
@@ -66,5 +72,61 @@ TEST(TestVegasCheckpoint, test_non_empty_checkpoint)
     chkpt2.serialize(stream2);
 
     // string representation should be the same
-    ASSERT_STREQ( stream1.str().c_str() , stream2.str().c_str() );
+    CHECK( stream1.str() == stream2.str() );
+}
+
+TEMPLATE_TEST_CASE("vegas_chkpt series", "", float, double, long double)
+{
+    using T = TestType;
+
+    // check if we get the same checkpoint when we perform five iterations in one go, and when we
+    // perfom first one iteration, then resume the resulting checkpoint with three iterations, and
+    // finally resume it with one iteration
+
+    auto const chkptA = hep::vegas(
+        hep::make_integrand<T>(
+            linear_function<T>,
+            1,
+            hep::make_dist_params<T>(10, T(0.0), T(1.0), "distribution #1")
+        ),
+        std::vector<std::size_t>(5, 1000)
+    );
+
+    auto const tmp_chkpt1 = hep::vegas(
+        hep::make_integrand<T>(
+            linear_function<T>,
+            1,
+            hep::make_dist_params<T>(10, T(0.0), T(1.0), "distribution #1")
+        ),
+        std::vector<std::size_t>(1, 1000)
+    );
+
+    auto const tmp_chkpt2 = hep::vegas(
+        hep::make_integrand<T>(
+            linear_function<T>,
+            1,
+            hep::make_dist_params<T>(10, T(0.0), T(1.0), "distribution #1")
+        ),
+        std::vector<std::size_t>(3, 1000),
+        tmp_chkpt1
+    );
+
+    auto const chkptB = hep::vegas(
+        hep::make_integrand<T>(
+            linear_function<T>,
+            1,
+            hep::make_dist_params<T>(10, T(0.0), T(1.0), "distribution #1")
+        ),
+        std::vector<std::size_t>(1, 1000),
+        tmp_chkpt2
+    );
+
+    // Check if both checkpoints are the same
+    std::ostringstream stream1;
+    std::ostringstream stream2;
+
+    chkptA.serialize(stream1);
+    chkptB.serialize(stream2);
+
+    CHECK( stream1.str() == stream2.str() );
 }
